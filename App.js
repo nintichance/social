d4e8c1ebec1043081081a34d3d556ba2fe0a3d2f
nintichance <mintidance@gmail.com>
2018-04-04 00:17:23 -0400

    Committing to pull

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-import Home from './components/Home'
+import Home from './src/components/Home'
 
 export default class App extends React.Component {
   render() {
---
+++ src/components/Home.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import { View, Text, Image} from 'react-native'
+
+class Home extends Component{
+    render(){
+        return(
+            <View>
+                <Text>HELOO</Text>
+            </View>
+        )
+    }
+}
+  
+export default Home
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+  })
+  
